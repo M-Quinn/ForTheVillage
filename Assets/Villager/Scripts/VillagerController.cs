e85975307c09f0ec98569fc7ddddce6e450602cf
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-03-10 19:38:03 -0400

    StateFacade and basic state pattern works so far

--- Assets/Villager/Scripts/VillagerController.cs
+++ Assets/Villager/Scripts/VillagerController.cs
@@ -1,13 +1,51 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class VillagerController : MonoBehaviour
+namespace ForTheVillage.Villager
 {
-    [SerializeField] NavMeshAgent _navMesh;
-    public GameObject targetObject;
-
-    void Start()
+    public class VillagerController : MonoBehaviour
     {
-        _navMesh.SetDestination(targetObject.transform.position);
+        [SerializeField] NavMeshAgent _navMesh;
+        public GameObject targetObject;
+
+
+        StateFacade _stateFacade = new StateFacade();
+        IState _waitForTaskState;
+        IState _goToResourceState;
+        IState _getResourceState;
+        IState _returnToVillageState;
+        IState _emptyInventoryState;
+
+        void Awake()
+        {
+            _waitForTaskState = new WaitForTask(Log);
+            _goToResourceState = new GoToResource(Log);
+        }
+
+
+        void Start()
+        {
+            _stateFacade.ChangeState(_waitForTaskState);
+
+            _navMesh.SetDestination(targetObject.transform.position);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                _stateFacade.ChangeState(_waitForTaskState);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                _stateFacade.ChangeState(_goToResourceState);
+            }
+
+            _stateFacade.Execute();
+        }
+
+        void Log(string msg) => Debug.Log(msg);
     }
 }
