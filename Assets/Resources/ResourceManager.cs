36b2cd402ad1c0a1f99979e04345a871b665c4f2
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-03-13 22:13:24 -0400

    began creating a resource spawner

---
+++ Assets/Resources/ResourceManager.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using ForTheVillage.Village;
+using UnityEngine;
+
+namespace ForTheVillage
+{
+    public class ResourceManager
+    {
+        List<VillageController> _villages = new List<VillageController>();
+        
+        
+    }
+}
